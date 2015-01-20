971b5b4b68984db0373c8fba1a5db1b247cdb8a0
jokata <jokatavr@gmail.com>
2015-01-20 17:41:18 +0200

    Added the empty project.

---
+++ sample/src/main/java/com/eftimoff/empty/MainActivity.java
@@ -0,0 +1,14 @@
+package com.eftimoff.empty;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
