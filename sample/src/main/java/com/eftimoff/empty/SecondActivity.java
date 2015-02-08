13c89f7836a2564e0709f54b2e934e7a4fde6c52
Georgi Eftimov <jokatavr@gmail.com>
2015-02-08 23:04:52 +0200

    Version 1.0.6.

--- sample/src/main/java/com/eftimoff/empty/SecondActivity.java
+++ sample/src/main/java/com/eftimoff/empty/SecondActivity.java
@@ -16,8 +16,8 @@ public class SecondActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         final PathView pathView = (PathView) findViewById(R.id.pathView);
-        final Path path = makeConvexArrow(50, 100);
-        pathView.setPath(path);
+//        final Path path = makeConvexArrow(50, 100);
+//        pathView.setPath(path);
         pathView.setFillAfter(true);
         pathView.useNaturalColors();
         pathView.setOnClickListener(new View.OnClickListener() {
